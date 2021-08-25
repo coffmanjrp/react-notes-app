1158a9f436e044d24848f9113e0891a9001e5020
coffmanjrp <coffmanjrp@gmail.com>
2021-08-24 18:32:30 -0700

    Implement add note function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import { useState } from 'react';
 import { FaPlus } from 'react-icons/fa';
+import Note from './components/Note';
 import './App.scss';
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  const addNewNote = () => {
+    setNotes([...notes, { id: notes.length + 1, text: '' }]);
+  };
+
   return (
     <div className="App">
-      <button className="add">
+      <button type="button" className="add" onClick={addNewNote}>
         <FaPlus /> Add note
       </button>
+      {notes.length > 0 &&
+        notes.map((note) => (
+          <Note key={note.id} note={note} notes={notes} setNotes={setNotes} />
+        ))}
     </div>
   );
 }
